518c4112c54bd7e291a314f7e57e2a9fd4e3e7fb
Stefanie Luu <luu.stefanie@gmail.com>
2019-03-26 23:18:12 -0700

    Updates

---
+++ precache-manifest.d3701daf2d5511826435ea952fd5c2f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f9214a410e37d2a4cf8",
+    "url": "/InsightGlobal-Walmart_CodingChallenge/static/js/runtime~main.854d25f9.js"
+  },
+  {
+    "revision": "e449d10c640a02a4fd78",
+    "url": "/InsightGlobal-Walmart_CodingChallenge/static/js/main.4b511807.chunk.js"
+  },
+  {
+    "revision": "c52b5364f1821694b68a",
+    "url": "/InsightGlobal-Walmart_CodingChallenge/static/js/2.4f9c1e2a.chunk.js"
+  },
+  {
+    "revision": "e449d10c640a02a4fd78",
+    "url": "/InsightGlobal-Walmart_CodingChallenge/static/css/main.dafd0678.chunk.css"
+  },
+  {
+    "revision": "033c500e9ddc228c83cf4a99582cf6e5",
+    "url": "/InsightGlobal-Walmart_CodingChallenge/index.html"
+  }
+];
